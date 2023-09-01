4aa7493a0aceb5bfa9e23b0b88cf88a766732919
tiago <tiagoandroloko@gmail.com>
2023-09-01 10:31:51 -0300

    criptografia de senhas adc

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -14,6 +15,11 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+});
+
+UserSchema.pre('save', async function(next) { 
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
 });
 
 const User = mongoose.model('User', UserSchema);
